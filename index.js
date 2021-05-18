c95586fae85698a71efb81864b0ca5ad6bb2c46d
Neeraj Yadav <ny29599@gmail.com>
2021-05-18 09:53:50 +0530

    📦 NEW: welcome message

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console')
+const welcome = require('cli-welcome')
+const pkgJson = require('./package.json')
 
-clearConsole()
+welcome({
+  title: pkgJson.name,
+  tagLine: `Nice to meet you`,
+  description: pkgJson.description,
+  version: pkgJson.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
+
 console.log(`Neeraj Kumar - Software Engineer`);
