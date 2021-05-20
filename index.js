823d501b2c7bc82d805acbd44ae744144a105eb9
Neeraj Yadav <ny29599@gmail.com>
2021-05-20 18:28:20 +0530

    👌 IMPROVE: add util/data for free up index

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 #!/usr/bin/env node
-const chalk = require('chalk')
 // const handleError = require('cli-handle-error')
+const alert = require('cli-alerts-codeword7')
 const log = console.log;
 const init = require('./utils/init')
-//Alerts
-const sym = require('log-symbols')
-const success = chalk.green.inverse
-const info = chalk.blue.inverse
-const warning = chalk.keyword(`orange`).inverse
-const error = chalk.red.bold.inverse
+const data = require('./utils/data')
 
 
 init()
-
-log(` ${chalk.bgBlue.bold(` Neeraj Kumar `)} - Software Engineer`);
-
-log(`
-        ${sym.success} ${success(` SUCCESS `)}: Thanks for checking out my CLI
-
-        ${sym.info} ${info(` INFO `)}: I'm creating a course on Udemy.com
-
-        ${sym.warning} ${warning(` WARNING `)}: Please don't copy me. Be yourself.
-
-        ${sym.error} ${error(` ERROR `)}: I'm on vacation. Contact me next week.
-        `)
+log(data.bio);
+log(data.social)
+alert({ type: 'info', msg: data.ad })
---
+++ utils/data.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk')
+
+const dim = chalk.dim;
+const italic = chalk.italic;
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
+const githubClr = chalk.hex(`#6cc644`).bold.inverse;
+const purple = chalk.hex(`#6937FF`).bold.inverse;
+
+const bio = `${italic(
+  `Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
+)}
+`;
+
+const social = `
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com/codeword007`)}
+${githubClr(` GitHub `)}  ${dim(`https://github.com/codeword7`)}
+${purple(` Blog `)}    ${dim(`https://github.com/codeword7`)}
+`;
+
+const ad = `Check out my Udemy.com course.`;
+const blogName = `codeword7.com`;
+const blog = `Latest posts`;
+
+module.exports = {
+  bio,
+  social,
+  ad,
+  blogName,
+  blog,
+}
