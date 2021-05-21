81205231d750a580d737abe582b71f05a7abbe34
Neeraj Yadav <ny29599@gmail.com>
2021-05-21 12:38:48 +0530

    📦 NEW: add spinner

--- utils/stats.js
+++ utils/stats.js
@@ -1,13 +1,20 @@
 const fetch = require('node-fetch');
+const { yellow } = require('chalk');
 const handleError = require('cli-handle-error')
+const ora = require('ora');
+
+const spinner = ora({ text: '' })
 const API_URL = `https://api.github.com/users/codeword7`
 module.exports = async () => {
   try {
+    spinner.start(`${yellow(`FOLLOWERS`)} fetching...`)
     const res = await fetch(API_URL)
     const data = await res.json()
     const ghFollowers = data.followers
+    // spinner.succeed(`${green(`FOLLOWERS`)} fetched`)
+    spinner.stop()
     console.log(`
-  Github Followers: ${ghFollowers}
+Github Followers: ${ghFollowers}
   `)
   } catch (error) {
     handleError(`API CALL FAILED`, error, false, false)
